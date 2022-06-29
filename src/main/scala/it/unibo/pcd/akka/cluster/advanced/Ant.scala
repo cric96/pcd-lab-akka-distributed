0b831a438ae5140d74211165598cd2bfcddad306
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-06-29 15:14:26 +0200

    chore: a workaround for registering and subscribing in the same actor

--- src/main/scala/it/unibo/pcd/akka/cluster/advanced/Ant.scala
+++ src/main/scala/it/unibo/pcd/akka/cluster/advanced/Ant.scala
@@ -1,10 +1,11 @@
 package it.unibo.pcd.akka.cluster.advanced
 
-import akka.actor.typed.receptionist.Receptionist
+import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
 import akka.actor.typed.scaladsl.*
 import akka.actor.typed.scaladsl.adapter.*
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import it.unibo.pcd.akka.Message
+import it.unibo.pcd.akka.cluster.advanced.AntsRender.Render
 
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Random
@@ -16,39 +17,51 @@ object Ant:
 
   sealed trait Command extends Message // Enum needs an ad-hoc serializers...
   case object Stop extends Command
+  private case class FrontendsRegistered(renders: List[ActorRef[AntsRender.Render]])
+      extends Command // ad-hoc message to handle fronted
   private case object Move extends Command // and ADT enable also private messages
+  val Key = ServiceKey[Stop.type]("Ant")
 
   def apply(
       position: (Int, Int),
       period: FiniteDuration,
       frontends: List[ActorRef[AntsRender.Render]] = List.empty
-  )(using random: Random): Behavior[Command | Receptionist.Listing] =
-    Behaviors.setup[Command | Receptionist.Listing] { ctx =>
-      ctx.system.receptionist ! Receptionist.Subscribe(AntsRender.Service, ctx.self) // register to new frontend
+  )(using random: Random): Behavior[Command] =
+    Behaviors.setup[Command] { ctx =>
+      ctx.spawnAnonymous(manageFrontend(ctx.self)) // spawn the logic that handles subscribes of new fronted.
+      ctx.system.receptionist ! Receptionist.Register(Ant.Key, ctx.self)
       Behaviors.withTimers { timers =>
         timers.startTimerAtFixedRate(Move, period)
         antLogic(position, ctx, frontends)
       }
     }
-
+  // Ad-hoc logic to handle subscribe
+  def manageFrontend(sendReplyTo: ActorRef[Command]): Behavior[Receptionist.Listing] = {
+    Behaviors.setup[Receptionist.Listing] { ctx =>
+      ctx.system.receptionist ! Receptionist.Subscribe(AntsRender.Service, ctx.self)
+      Behaviors.receiveMessage { case msg: Receptionist.Listing =>
+        sendReplyTo ! FrontendsRegistered(msg.serviceInstances(AntsRender.Service).toList)
+        Behaviors.same
+      }
+    }
+  }
   // Main logic, each period the ant changes its position (using the random generator in the context)
   def antLogic(
       position: (Int, Int),
-      ctx: ActorContext[Command | Receptionist.Listing],
+      ctx: ActorContext[Command],
       frontends: List[ActorRef[AntsRender.Render]] = List.empty
-  )(using random: Random): Behavior[Command | Receptionist.Listing] = Behaviors.receiveMessage {
-    case msg: Receptionist.Listing =>
-      ctx.log.info(s"New frontend! $msg")
-      val services = msg.serviceInstances(AntsRender.Service).toList
-      if (services == frontends)
+  )(using random: Random): Behavior[Command] = Behaviors.receiveMessage {
+    case FrontendsRegistered(newRenders) =>
+      ctx.log.info(s"New frontend! $newRenders")
+      if (newRenders == frontends)
         Behaviors.same
       else
-        antLogic(position, ctx, msg.serviceInstances(AntsRender.Service).toList)
+        antLogic(position, ctx, newRenders)
     case Move =>
       val (deltaX, deltaY) = ((random.nextGaussian * 5).toInt, (random.nextGaussian * 5).toInt)
       val (x, y) = position
       frontends.foreach(_ ! AntsRender.Render(x, y, ctx.self))
-      ctx.log.info(s"move from ${(x, y)}, ${ctx.self.path}")
+      //ctx.log.info(s"move from ${(x, y)}, ${ctx.self.path}")
       antLogic((x + deltaX, y + deltaY), ctx, frontends)
     case Stop => Behaviors.stopped
   }
